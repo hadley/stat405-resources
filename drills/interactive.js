e2838c0f5059b392a32e3d98fdee065c9aef1b31
hadley <h.wickham@gmail.com>
2009-10-07 13:18:07 -0500

    Javascript for hints

--- drills/interactive.js
+++ drills/interactive.js
@@ -1,10 +1,23 @@
 $(document).ready(function() {
-    $("pre.answer").
-      css("display", "none")
-    $("li p").
+  
+    // Hide hints & provide ability to show them
+    $("li p.hint").wrapInner("<span></span>").
+      prepend("<a class='toggle-hint' href='#'>Show hint</a>.  ");
+
+    $("li p.hint span").css("display", "none");
+
+    $("a.toggle-hint").click(function() {
+      hint = $(this).closest("p").find("span");
+      hint.css("display", "inline");
+      return(false);
+    });
+
+    // Hide answers, and provide link to show them.
+    $("pre.answer").css("display", "none")
+    $("li p:first-child").
       append("<span>  <a class='toggle' href='#'>Show answer</a>.</span>");
     $("a.toggle").click(function() {
-      code = $(this).parent().parent().next();
+      code = $(this).closest("li").find("pre");
       if (code.css("display") == "none") {
         code.slideDown();
         $(this).text("Hide answer");          
