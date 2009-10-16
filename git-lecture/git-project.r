bc96c21be95774674a2c9b9ce14150c1db6dee53
garrettgman <grolemund@gmail.com>
2009-10-16 14:58:11 -0500

    improved code

--- git-lecture/git-project.r
+++ git-lecture/git-project.r
@@ -1,19 +1,17 @@
 # git project
 
-names <- read.csv("baby-names.csv", header = T)
+names <- read.csv("baby-names.csv", header = T, stringsAsFactors = F)
 
 # extracting all data related to my name
 my_name <- subset(names, name == "Garrett" & sex == "boy")
 
 # plotting my name's popularity over time
 library(ggplot2)
-qplot(year, percent, data = my_name, geom = "line")
+qplot(year, percent, data = my_name, geom = "line", main = "Percentage of children named Garrett, by year")
 
 
 # for later
 both_names <- rbind(my_name, second_name)
-# this step isn't necessary, but will save MUCH time when we graph
-both_names$name <- factor(both_names$name)
 
 qplot(year, percent, data = both_names, geom = "line", colour = name, main = "Popularity of group member names over time")
-ggsave("names.png", width = 6, height = 6)
+ggsave("names.pdf", width = 5, height = 4)
