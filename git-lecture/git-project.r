8f317d9e7635b3f681ef6c8ab6442bb53fa5139e
garrettgman <grolemund@gmail.com>
2009-10-23 17:54:32 -0500

    changed percent to prop

--- git-lecture/git-project.r
+++ git-lecture/git-project.r
@@ -7,11 +7,11 @@ my_name <- subset(names, name == "Garrett" & sex == "boy")
 
 # plotting my name's popularity over time
 library(ggplot2)
-qplot(year, percent, data = my_name, geom = "line", main = "Percentage of children named Garrett, by year")
+qplot(year, prop, data = my_name, geom = "line", main = "Percentage of children named Garrett, by year")
 
 
 # for later
 both_names <- rbind(my_name, second_name)
 
-qplot(year, percent, data = both_names, geom = "line", colour = name, main = "Popularity of group member names over time")
+qplot(year, prop, data = both_names, geom = "line", colour = name, main = "Popularity of group member names over time")
 ggsave("names.pdf", width = 5, height = 4)
